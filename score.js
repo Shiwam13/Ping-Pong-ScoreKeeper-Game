2709403ee36dcd7e6633ed5befaf89fcd762b4d9
Shiwam13 <110454241+Shiwam13@users.noreply.github.com>
2022-08-02 15:21:41 +0530

    Ping-Pong -ScoreKeeper-Game

---
+++ score.js
@@ -0,0 +1,53 @@
+const p1button=document.querySelector('#p1button');
+const p2button=document.querySelector('#p2button');
+const p1display=document.querySelector('#p1display');
+const p2display=document.querySelector('#p2display');
+const resetbutton=document.querySelector('#reset');
+const winningscoreselect=document.querySelector('#playto');
+
+
+let p1score=0;
+let p2score= 0;
+let winscore=5;
+let isgameover=false;
+
+
+p1button.addEventListener('click',function(){
+    if(!isgameover){
+    p1score+=1;
+    if(p1score===winscore){
+        isgameover=true;
+    p1display.classList.add('winner');
+p2display.classList.add('looser');
+}
+p1display.textContent=p1score;}
+
+})
+p2button.addEventListener('click',function(){
+    if(!isgameover){
+    p2score+=1;
+    if(p2score===winscore){
+        isgameover=true;
+        p2display.classList.add('winner');
+        p1display.classList.add('looser');}
+p2display.textContent=p2score;}
+
+}
+)
+winningscoreselect.addEventListener('change',function(){
+    winscore=parseInt(this.value);
+    reset();
+ })
+resetbutton.addEventListener('click',reset)
+
+
+function reset() {
+    isgameover=false
+    p1score=0;
+    p2score=0;
+    p1display.textContent=0;
+    p2display.textContent=0;
+    p1display.classList.remove('winner','looser');
+    p2display.classList.remove('winner','looser');
+
+}
